f1bf935a129d6d80dcc85cc16bb029acbdfde188
zhangjingfei <316003394@qq.com>
2015-07-25 18:41:45 +0800

    ~

---
+++ shipin/Tool/Tool.h
@@ -0,0 +1,13 @@
+//
+//  Tool.h
+//  shipin
+//
+//  Created by Mapollo27 on 15/7/25.
+//  Copyright (c) 2015年 dust.zhang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Tool : NSObject
+
+@end
---
+++ shipin/Tool/Tool.m
@@ -0,0 +1,13 @@
+//
+//  Tool.m
+//  shipin
+//
+//  Created by Mapollo27 on 15/7/25.
+//  Copyright (c) 2015年 dust.zhang. All rights reserved.
+//
+
+#import "Tool.h"
+
+@implementation Tool
+
+@end
